44960fd3d44ef292464731826e7ec50550e24f13
nugroho pangestu <nugrohopangestu@MacBook-Air-nugroho.local>
2019-11-30 12:58:45 +0800

    change uuid data type

--- database/migrations/2019_11_30_034819_create_karyawans_table.php
+++ database/migrations/2019_11_30_034819_create_karyawans_table.php
@@ -16,7 +16,7 @@ class CreateKaryawansTable extends Migration
         Schema::create('karyawans', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedbigInteger('user_id');
-            $table->string('uuid')->length(100);
+            $table->text('uuid')->nullable();
             $table->string('NIP')->length('25');
             $table->string('tempat_lahir')->length(255);
             $table->date('tanggal_lahir');
