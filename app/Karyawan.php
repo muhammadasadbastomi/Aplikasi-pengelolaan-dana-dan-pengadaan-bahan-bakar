35ce7ca4211c55b2bbea50ca929810c174fe84f6
nugroho pangestu <nugrohopangestu@MacBook-Air-nugroho.local>
2019-11-30 13:04:28 +0800

    create method fillable, hidden and relation table

--- app/Karyawan.php
+++ app/Karyawan.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Karyawan extends Model
 {
-    //
+    protected $fillable = [
+        'NIP', 'tempat_lahir', 'tanggal_lahir', 'alamat', 'telepon'
+    ];
+    protected $hidden = [
+        'id', 'user_id'
+    ];
+    
+    public function user(){
+      return $this->HasOne('App\User');
+    }
+}
 }
