c8f5832be4c0e0d6270cd11b6baec9bd33adde1c
nugroho pangestu <nugrohopangestu@MacBook-Air-nugroho.local>
2019-11-30 13:16:51 +0800

    initialize required library

--- app/Http/Controllers/KaryawanController.php
+++ app/Http/Controllers/KaryawanController.php
@@ -1,10 +1,15 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
+use Illuminate\Support\Facades\Redis;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Karyawan;
+use App\User;
+use HCrypt;
 
-class KaryawanController extends Controller
+class KaryawanController extends APIController
 {
     //
 }
