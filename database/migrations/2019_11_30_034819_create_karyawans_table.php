d1a62015a6c9c10a82af396e13f07e3b85fc11f2
nugroho pangestu <nugrohopangestu@MacBook-Air-nugroho.local>
2019-11-30 12:56:58 +0800

    add field karyawan migration

--- database/migrations/2019_11_30_034819_create_karyawans_table.php
+++ database/migrations/2019_11_30_034819_create_karyawans_table.php
@@ -15,6 +15,14 @@ class CreateKaryawansTable extends Migration
     {
         Schema::create('karyawans', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedbigInteger('user_id');
+            $table->string('uuid')->length(100);
+            $table->string('NIP')->length('25');
+            $table->string('tempat_lahir')->length(255);
+            $table->date('tanggal_lahir');
+            $table->string('alamat')->length(255);
+            $table->string('telepon')->length(13);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
