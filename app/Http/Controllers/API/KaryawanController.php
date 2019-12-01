e6c3d806477da14599257d21770c0ba237834b23
nugroho pangestu <nugrohopangestu@MacBook-Air-nugroho.local>
2019-12-02 01:31:18 +0800

    add method update karyawan include user

--- app/Http/Controllers/API/KaryawanController.php
+++ app/Http/Controllers/API/KaryawanController.php
@@ -67,5 +67,50 @@ class KaryawanController extends APIController
         return $this->returnController("ok", $merge);
     }
 
+    public function update($uuid, Request $req){
+        $id = HCrypt::decrypt($uuid);
+        if (!$id) {
+            return $this->returnController("error", "failed decrypt uuid");
+        }
+        $karyawan = karyawan::findOrFail($id);
+        $user_id = $karyawan->user_id;
+        $user = User::findOrFail($user_id);
+        if (!$user){
+                return $this->returnController("error", "failed find data karyawan");
+            }
+        if($req->foto != null){
+                $FotoExt  = $req->foto->getClientOriginalExtension();
+                $FotoName = $req->user_id.' - '.$req->name;
+                $foto   = $FotoName.'.'.$FotoExt;
+                $req->foto->move('images/user', $foto);
+                $user->foto       = $foto;
+                }else {
+                    $user->foto  = $user->foto;
+                }
+            $user->name            = $req->name;
+            $user->email    = $req->email;
+            if($req->password != null){
+            $password       = Hash::make($req->password);
+            $user->password = $password;
+            }else{
+            }
+           $user->update();
+           $karyawan->NIP     = $req->NIP;
+           $karyawan->tempat_lahir    = $req->tempat_lahir;
+           $karyawan->tanggal_lahir    = $req->tanggal_lahir;
+           $karyawan->alamat    = $req->alamat;
+           $karyawan->telepon    = $req->telepon;
+           $karyawan->update();
+        if (!$user && $karyawan) {
+            return $this->returnController("error", "failed find data karyawan");
+        }
+        $merge = (['user' => $user, 'karyawan' => $karyawan]);
+        Redis::del("user:all");
+        Redis::set("user:$user_id", $user);
+        Redis::del("karyawan:all");
+        Redis::set("karyawan:$id", $karyawan);
+        return $this->returnController("ok", $merge);
+    }
+
 
 }
