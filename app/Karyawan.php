592bba01fc6c9d12f2e067437842b349e40229fa
nugroho pangestu <nugrohopangestu@MacBook-Air-nugroho.local>
2019-12-02 01:15:43 +0800

    delete fillable status

--- app/Karyawan.php
+++ app/Karyawan.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Karyawan extends Model
 {
     protected $fillable = [
-        'NIP', 'tempat_lahir', 'tanggal_lahir', 'alamat', 'telepon'
+       'uuid', 'NIP', 'tempat_lahir', 'tanggal_lahir', 'alamat', 'telepon'
     ];
     protected $hidden = [
         'id', 'user_id'
