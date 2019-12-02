3d0acb7c6968a7f77c7999f95208c29dae65c8c3
nugroho pangestu <nugrohopangestu@MacBook-Air-nugroho.local>
2019-12-02 21:53:55 +0800

    initialize library seksi controller

--- app/Http/Controllers/API/SeksiController.php
+++ app/Http/Controllers/API/SeksiController.php
@@ -1,10 +1,16 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Bidang;
+use App\Seksi;
+use HCrypt;
 
-class SeksiController extends Controller
+class SeksiController extends APIController
 {
     //
 }
