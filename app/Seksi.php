01cbed3f625ddab0bcc52b7307fd919714ec3522
nugroho pangestu <nugrohopangestu@MacBook-Air-nugroho.local>
2019-12-02 21:42:42 +0800

    add fillable,  hidden and relation into bidang

--- app/Seksi.php
+++ app/Seksi.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Seksi extends Model
 {
-    //
+    protected $fillable = [
+        'uuid','kode_seksi','nama',
+    ];
+
+    protected $hidden = [
+        'id','bidang_id'
+    ];
+
+    public function bidang()
+    {
+        return $this->HasMany('App\Bidang');
+    }
 }
