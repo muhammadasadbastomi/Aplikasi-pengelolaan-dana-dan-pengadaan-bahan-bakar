ac285e00ac4b594fd47d01245597494bd1a593be
nugroho pangestu <nugrohopangestu@MacBook-Air-nugroho.local>
2019-12-02 10:06:16 +0800

    add method get data kendaraan

--- app/Http/Controllers/API/KendaraanController.php
+++ app/Http/Controllers/API/KendaraanController.php
@@ -1,10 +1,26 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
+use Illuminate\Support\Facades\Redis;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Kendaraan;
+use HCrypt;
 
-class KendaraanController extends Controller
+class KendaraanController extends APIController
 {
-    //
+    public function get(){
+    $kendaraan = json_decode(redis::get("kendaraan::all"));
+    if (!$kendaraan) {
+        $kendaraan = kendaraan::all();
+        if (!$kendaraan) {
+            return $this->returnController("error", "failed get kendaraan data");
+        }
+        Redis::set("kendaraan:all", $kendaraan);
+    }
+    return $this->returnController("ok", $kendaraan);
+    }
+
+    
 }
