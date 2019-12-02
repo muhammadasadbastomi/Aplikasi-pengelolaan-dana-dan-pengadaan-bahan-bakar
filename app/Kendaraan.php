0186b6dea92e099e03dc8b15f91b47011fcef1ea
nugroho pangestu <nugrohopangestu@MacBook-Air-nugroho.local>
2019-12-02 10:25:43 +0800

    change karyawan_id into filabel

--- app/Kendaraan.php
+++ app/Kendaraan.php
@@ -7,11 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Kendaraan extends Model
 {
     protected $fillable = [
-        'uuid', 'nopol', 'merk', 'tipe', 'jenis', 'model', 'tahun_pembuatan', 'isi_silinder', 'warna_kendaraan',
+        'karyawan_id','uuid', 'nopol', 'merk', 'tipe', 'jenis', 'model', 'tahun_pembuatan', 'isi_silinder', 'warna_kendaraan',
         'bahan_bakar', 'warna_tnkb', 'tahun_registrasi', 'no_mesin', 'no_bpkb', 'tercatat_kib',
      ];
      protected $hidden = [
-         'id', 'karyawan_id'
+         'id', 
      ];
  
      public function karyawan(){
