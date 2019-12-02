69f9017b1d5100c98d88de4b617c765f5d670030
nugroho pangestu <nugrohopangestu@MacBook-Air-nugroho.local>
2019-12-02 10:06:00 +0800

    add fillable, hidden and relation tabel model kendaraan

--- app/Kendaraan.php
+++ app/Kendaraan.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Kendaraan extends Model
 {
-    //
+    protected $fillable = [
+        'uuid', 'nopol', 'merk', 'tipe', 'jenis', 'model', 'tahun_pembuatan', 'isi_silinder', 'warna_kendaraan',
+        'bahan_bakar', 'warna_tnkb', 'tahun_registrasi', 'no_mesin', 'no_bpkb', 'tercatat_kib',
+     ];
+     protected $hidden = [
+         'id', 'karyawan_id'
+     ];
+ 
+     public function karyawan(){
+       return $this->HasOne('App\Karyawan');
+     }
+
 }
