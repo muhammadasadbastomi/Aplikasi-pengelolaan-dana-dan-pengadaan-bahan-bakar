fcd449dc1a70db66996a3863f499599ce67518ec
nugroho pangestu <nugrohopangestu@MacBook-Air-nugroho.local>
2019-12-02 21:03:00 +0800

    create CRUD method bidang controller

--- app/Http/Controllers/API/BidangController.php
+++ app/Http/Controllers/API/BidangController.php
@@ -11,5 +11,85 @@ use HCrypt;
 
 class BidangController extends APIController
 {
-    //
+    public function get(){
+    $bidang = json_decode(redis::get("bidang::all"));
+    if (!$bidang) {
+        $bidang = bidang::all();
+        if (!$bidang) {
+            return $this->returnController("error", "failed get bidang data");
+        }
+        Redis::set("bidang:all", $bidang);
+    }
+    return $this->returnController("ok", $bidang);
+    }
+
+    public function find($uuid){
+        $id = HCrypt::decrypt($uuid);
+        if (!$id) {
+            return $this->returnController("error", "failed decrypt uuid");
+        }
+        $bidang = Redis::get("bidang:$id");
+        if (!$bidang) {
+            $bidang = bidang::find($id);
+            if (!$bidang){
+                return $this->returnController("error", "failed find data bidang");
+            }
+            Redis::set("bidang:$id", $bidang);
+        }
+        return $this->returnController("ok", $bidang);
+    }
+
+    public function create(Request $req){
+        $bidang = bidang::create($req->all());
+        //set uuid
+        $bidang_id = $bidang->id;
+        $uuid = HCrypt::encrypt($bidang_id);
+        $setuuid = bidang::findOrFail($bidang_id);
+        $setuuid->uuid = $uuid;
+        $setuuid->update();
+        if (!$bidang) {
+            return $this->returnController("error", "failed create data bidang");
+        }
+        Redis::del("bidang:all");
+        return $this->returnController("ok", $bidang);
+    }
+
+    public function update($uuid, Request $req){
+        $id = HCrypt::decrypt($uuid);
+        if (!$id) {
+            return $this->returnController("error", "failed decrypt uuid");
+        }
+        $bidang = bidang::findOrFail($id);
+
+        $bidang->kode_bidang    = $req->kode_bidang;
+        $bidang->nama    = $req->nama;
+        
+        $bidang->update();
+        if (!$bidang) {
+            return $this->returnController("error", "failed find data bidang");
+        }
+        Redis::del("bidang:all");
+        Redis::set("bidang:$id", $bidang);
+        return $this->returnController("ok", $bidang);
+    }
+
+    public function delete($uuid){
+        $id = HCrypt::decrypt($uuid);
+        if (!$id) {
+            return $this->returnController("error", "failed decrypt uuid");
+        }
+        $bidang = bidang::findOrFail($id);
+        if (!$bidang) {
+            return $this->returnController("error", "failed find data bidang");
+        }
+        // Need to check realational
+        // If there relation to other data, return error with message, this data has relation to other table(s)
+        $delete = $bidang->delete();
+        if (!$delete) {
+            return $this->returnController("error", "failed delete data bidang");
+        }
+        Redis::del("bidang:all");
+        Redis::del("bidang:$id");
+        return $this->returnController("ok", "success delete data bidang");
+    }
 }
