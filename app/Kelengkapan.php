cfb7513fc8555e640279218052ced1036df55aa9
muhammadasadbastomi <tomycules@gmail.com>
2019-12-04 14:07:19 +0700

    add fillable and hidden method

--- app/Kelengkapan.php
+++ app/Kelengkapan.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Kelengkapan extends Model
 {
-    //
+    protected $fillable = [
+        'uuid','kendaraan_id','item_kendaraan_id','status'
+    ];
+
+    protected $hidden = [
+        'id','kendaraan_id','item_kendaraan_id'
+    ];
 }
