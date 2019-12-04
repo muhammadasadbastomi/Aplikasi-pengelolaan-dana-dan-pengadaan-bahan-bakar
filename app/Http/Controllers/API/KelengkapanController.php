cf5e1df62e14c1959456a10d20d53fb8b20ee6d9
muhammadasadbastomi <tomycules@gmail.com>
2019-12-04 21:00:57 +0800

    fix kelengkapan model not found

--- app/Http/Controllers/API/KelengkapanController.php
+++ app/Http/Controllers/API/KelengkapanController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\API;
 use Illuminate\Support\Facades\Redis;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Kendaraan;
+use App\Kelengkapan;
 use HCrypt;
 
 class KelengkapanController extends APIController
