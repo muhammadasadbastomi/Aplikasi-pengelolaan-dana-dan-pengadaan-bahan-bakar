8b90d8cda27622c589231b61be3acd77bdaedede
muhammadasadbastomi <tomycules@gmail.com>
2019-12-04 21:24:06 +0800

    add create method kelengkapan controller

--- app/Http/Controllers/API/KelengkapanController.php
+++ app/Http/Controllers/API/KelengkapanController.php
@@ -38,5 +38,23 @@ class KelengkapanController extends APIController
         return $this->returnController("ok", $kelengkapan);
     }
 
-    
+    public function create(Request $req){
+        $kelengkapan = new kelengkapan;
+        $kelengkapan->status = $req->status;
+        // decrypt kendaraan & item kendaraan id
+        $kelengkapan->kendaraan_id = Hcrypt::decrypt($req->kendaraan_id);
+        $kelengkapan->item_kendaraan_id = Hcrypt::decrypt($req->item_kendaraan_id);
+        $kelengkapan->save();
+        //set uuid
+        $kelengkapan_id = $kelengkapan->id;
+        $uuid = HCrypt::encrypt($kelengkapan_id);
+        $setuuid = kelengkapan::findOrFail($kelengkapan_id);
+        $setuuid->uuid = $uuid;
+        $setuuid->update();
+        if (!$kelengkapan) {
+            return $this->returnController("error", "failed create data kelengkapan");
+        }
+        Redis::del("kelengkapan:all");
+        return $this->returnController("ok", $kelengkapan);
+    }
 }
