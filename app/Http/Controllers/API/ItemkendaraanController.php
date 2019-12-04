ee0d02860278c967dd125366a5e7f7cff75d4f73
muhammadasadbastomi <tomycules@gmail.com>
2019-12-04 13:40:01 +0700

    delete unused library for item kendaraan controller

--- app/Http/Controllers/API/ItemkendaraanController.php
+++ app/Http/Controllers/API/ItemkendaraanController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\API;
 
 use Illuminate\Support\Facades\Redis;
-use Illuminate\Support\Facades\Hash;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Item_kendaraan;
