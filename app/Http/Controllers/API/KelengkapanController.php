09d8435183e98983b66036bc5f864dd44ed47cfe
muhammadasadbastomi <tomycules@gmail.com>
2019-12-04 20:54:14 +0800

    initialize require library kelengkapan controller

--- app/Http/Controllers/API/KelengkapanController.php
+++ app/Http/Controllers/API/KelengkapanController.php
@@ -1,10 +1,14 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
+use Illuminate\Support\Facades\Redis;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Kendaraan;
+use HCrypt;
 
-class KelengkapanController extends Controller
+class KelengkapanController extends APIController
 {
     //
 }
