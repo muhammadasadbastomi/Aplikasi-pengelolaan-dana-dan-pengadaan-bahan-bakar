5c378f577996cb76137e6d11ac8ac227f3816da0
muhammadasadbastomi <tomycules@gmail.com>
2019-12-04 13:39:05 +0700

    initialize require library item kendaraan

--- app/Http/Controllers/API/ItemkendaraanController.php
+++ app/Http/Controllers/API/ItemkendaraanController.php
@@ -1,10 +1,15 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Item_kendaraan;
+use HCrypt;
 
-class ItemkendaraanController extends Controller
+class ItemkendaraanController extends APIController
 {
     //
 }
