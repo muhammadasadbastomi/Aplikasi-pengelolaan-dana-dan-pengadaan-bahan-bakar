efcd8b9af8a61e57562caf7594c8a26d2e1bdc7f
muhammadasadbastomi <tomycules@gmail.com>
2019-12-04 13:44:48 +0700

    create get method item kendaraan

--- app/Http/Controllers/API/ItemkendaraanController.php
+++ app/Http/Controllers/API/ItemkendaraanController.php
@@ -10,5 +10,15 @@ use HCrypt;
 
 class ItemkendaraanController extends APIController
 {
-    //
+    public function get(){
+        $item_kendaraan = json_decode(redis::get("item_kendaraan::all"));
+        if (!$item_kendaraan) {
+            $item_kendaraan = item_kendaraan::all();
+            if (!$item_kendaraan) {
+                return $this->returnController("error", "failed get item_kendaraan data");
+            }
+            Redis::set("item_kendaraan:all", $item_kendaraan);
+        }
+        return $this->returnController("ok", $item_kendaraan);
+    }
 }
