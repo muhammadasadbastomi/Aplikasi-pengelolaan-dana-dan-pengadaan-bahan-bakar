64faf791e9bdab83b6f239f375c7f025380cf03d
muhammadasadbastomi <tomycules@gmail.com>
2019-12-04 13:50:14 +0700

    add method find item kendaraan

--- app/Http/Controllers/API/ItemkendaraanController.php
+++ app/Http/Controllers/API/ItemkendaraanController.php
@@ -21,4 +21,20 @@ class ItemkendaraanController extends APIController
         }
         return $this->returnController("ok", $item_kendaraan);
     }
+
+    public function find($uuid){
+        $id = HCrypt::decrypt($uuid);
+        if (!$id) {
+            return $this->returnController("error", "failed decrypt uuid");
+        }
+        $item_kendaraan = Redis::get("item_kendaraan:$id");
+        if (!$item_kendaraan) {
+            $item_kendaraan = item_kendaraan::find($id);
+            if (!$item_kendaraan){
+                return $this->returnController("error", "failed find data item_kendaraan");
+            }
+            Redis::set("item_kendaraan:$id", $item_kendaraan);
+        }
+        return $this->returnController("ok", $item_kendaraan);
+    }
 }
