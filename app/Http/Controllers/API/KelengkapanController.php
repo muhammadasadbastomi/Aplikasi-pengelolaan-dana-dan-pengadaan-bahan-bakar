c5bb421d70fd1952b65a754de586a2ca1d47521b
muhammadasadbastomi <tomycules@gmail.com>
2019-12-09 22:16:48 +0800

    add delete and update method on kelengkapancontroller

--- app/Http/Controllers/API/KelengkapanController.php
+++ app/Http/Controllers/API/KelengkapanController.php
@@ -60,4 +60,45 @@ class KelengkapanController extends APIController
         Redis::set("kelengkapan:all", $kelengkapan);
         return $this->returnController("ok", $kelengkapan);
     }
+
+    public function update($uuid, Request $req){
+        $id = HCrypt::decrypt($uuid);
+        if (!$id) {
+            return $this->returnController("error", "failed decrypt uuid");
+        }
+        $kelengkapan = kelengkapan::findOrFail($id);
+        $kendaraan_id = Hcrypt::decrypt($req->kendaraan_id);
+        $kelengkapan->kendaraan_id     = $kendaraan_id;
+        $kelengkapan->kendaraan_id = $kendaraan_id;
+        $kelengkapan->item_kendaraan_id = $item_kendaraan_id;
+        
+        $kelengkapan->update();
+        if (!$kelengkapan) {
+            return $this->returnController("error", "failed find data kelengkapan");
+        }
+        $kelengkapan = kelengkapan::with('karyawan')->where('id',$id)->first();
+        Redis::del("kelengkapan:all");
+        Redis::set("kelengkapan:$id", $kelengkapan);
+        return $this->returnController("ok", $kelengkapan);
+    }
+
+    public function delete($uuid){
+        $id = HCrypt::decrypt($uuid);
+        if (!$id) {
+            return $this->returnController("error", "failed decrypt uuid");
+        }
+        $kelengkapan = kelengkapan::findOrFail($id);
+        if (!$kelengkapan) {
+            return $this->returnController("error", "failed find data kelengkapan");
+        }
+        // Need to check realational
+        // If there relation to other data, return error with message, this data has relation to other table(s)
+        $delete = $kelengkapan->delete();
+        if (!$delete) {
+            return $this->returnController("error", "failed delete data kelengkapan");
+        }
+        Redis::del("kelengkapan:all");
+        Redis::del("kelengkapan:$id");
+        return $this->returnController("ok", "success delete data kelengkapan");
+    }
 }
