6c3a09400a0d395f8ccac53a98aff1025e0aa979
tri angga utama <trianggategarutama@gmail.com>
2020-02-10 11:22:47 +0800

    tambah ubah rincian

--- resources/views/admin/pencairan/detail.blade.php
+++ resources/views/admin/pencairan/detail.blade.php
@@ -36,6 +36,7 @@
                                     <th>Satuan</th>
                                     <th>Harga Satuan</th>
                                     <th>Total</th>
+                                    <th>Aksi</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -49,6 +50,7 @@
                                     <th>Satuan</th>
                                     <th>Harga Satuan</th>
                                     <th>Total</th>
+                                    <th>Aksi</th>
                                 </tr>
                             </tfoot>
                         </table>
@@ -61,10 +63,71 @@
     </div>
 </div>
 </div>
+<!-- modal -->
+
+<!-- modal -->
+<div class="modal fade" id="mediumModal"  role="dialog"  >
+                    <div class="modal-dialog modal-lg" >
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="mediumModalLabel">Tambah Pencairan</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                            <form action="" enctype="multipart/form-data" method="post">
+                            <input type="hidden" name="uuid" id="uuid">
+                            <div class="form-group">
+                                <label for="">Nama item</label>
+                                <input class="form-control" type="text" name="nama_item" id="nama_item">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Satuan</label>
+                                <input class="form-control" type="text" name="satuan" id="satuan">
+                            </div>                            
+                            <div class="form-group">
+                                <label for="">Harga Satuan</label>
+                                <input class="form-control" type="text" name="harga_satuan" id="harga_satuan">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Volume</label>
+                                <input class="form-control" type="text" name="volume" id="volume">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Tanggal Transaksi</label>
+                                <input class="form-control" type="date" name="tanggal_transaksi" id="tanggal_transaksi">
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn " data-dismiss="modal"> <i class="ti-close"></i> Batal</button>
+                                <button type="submit" class="btn btn-primary"><i class="ti-save"></i> Simpan</button>
+                                {{@csrf_field()}}
+                            </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 @endsection
 @section('script')
     <script>
-
+            edit = uuid =>{
+                $.ajax({
+                    type: "GET",
+                    url: "{{ url('/api/rincian')}}" + '/' + uuid,
+                    beforeSend: false,
+                    success : function(returnData) {
+                        $('.modal-title').text('Edit Data');
+                        $('#uuid').val(returnData.data.uuid);
+                        $('#nama_item').val(returnData.data.nama_item);
+                        $('#satuan').val(returnData.data.satuan);
+                        $('#harga_satuan').val(returnData.data.harga_satuan);  
+                        $('#volume').val(returnData.data.volume);
+                        $('#tanggal_transaksi').val(returnData.data.tanggal_transaksi);    
+                        $('#btn-form').text('Ubah Data');
+                        $('#mediumModal').modal('show');
+                    }
+                })
+            }
           //fungsi render datatable            
           $(document).ready(function() {
                 let id = $('#pencairan_id').val();
@@ -91,11 +154,43 @@
                             }},
                         {data: null , render : function ( data, type, row, meta ) {
                             return '<p> Rp.'+ row.total_harga_item +'<p>';
-                            }},  
+                            }},
+                        {data: null , render : function ( data, type, row, meta ) {
+                            let uuid = row.uuid;
+                            return type === 'display'  ?
+                            '<button onClick="edit(\''+uuid+'\')" class="btn btn-sm btn-outline-primary" data-toggle="modal" data-target="#editmodal"><i class="ti-pencil"></i>':
+                        data;
+                        }}  
                     ]
                 });
             });
-
-              
+            
+              //event form submit
+              $("form").submit(function (e) {
+                    e.preventDefault()
+                        let form = $('#modal-body form');
+                        let url = '{{route("API.rincian.update", '')}}'
+                        let id = $('#uuid').val();
+                        $.ajax({
+                            url: url+'/'+id,
+                            type: "put",
+                            data: $(this).serialize(),
+                            success: function (response) {
+                                form.trigger('reset');
+                                $('#mediumModal').modal('hide');
+                                $('#datatable').DataTable().ajax.reload();
+                                Swal.fire({
+                                    position: 'top-end',
+                                    icon: 'success',
+                                    title: 'Data Berhasil Diubah',
+                                    showConfirmButton: false,
+                                    timer: 1500
+                                })
+                            },
+                            error:function(response){
+                                console.log(response);
+                            }
+                        })
+                } );
     </script>
 @endsection
